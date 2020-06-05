ae9dcdfbf8a3f032a8f38c86be4c06eebcb23d94
ielay <ielay@yandex.ru>
2020-06-06 01:45:35 +0300

    Some changes for Heroku

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const compression = require('compression');
+// const compression = require('compression');
 
 const app = express();
 
-app.use(compression());
+// app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'dist/dailyplanner-front')));
 
